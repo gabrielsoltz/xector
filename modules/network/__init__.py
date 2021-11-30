ed9b664029b4b032dd4c0491f477c983a35ff3f3
gabrielsoltz <thegaby@gmail.com>
2021-11-30 21:28:03 +0100

    improve_network

--- modules/network/__init__.py
+++ modules/network/__init__.py
@@ -5,7 +5,7 @@ class NetworkModule:
     def __init__(self, url):
         whois = self.get_whois(url)
         self.domain_name = whois.domain_name
-        self.name_servers = whois.name_servers
+        self.name_servers = self.get_name_servers(whois)
         self.registrar = whois.registrar
         self.registrar_url = whois.registrar_url
         self.status = whois.status
@@ -18,3 +18,14 @@ class NetworkModule:
     def get_whois(self, url):
         whois_info = whois.whois(url)
         return whois_info
+    
+    def get_name_servers(self, whois):
+        if '\n' in whois.name_servers:
+            name_servers = []
+            for line in whois.name_servers.splitlines():
+                line = line.strip()
+                if line != "":
+                    name_servers.append(line)
+        else:
+            return whois.name_servers
+        return name_servers
